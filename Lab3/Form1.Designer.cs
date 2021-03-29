377328a86d1d0def56d9fdcc0e96787d1b9130c6
Петр Данилов <danilov.pa@edu.spbstu.ru>
2021-03-29 19:57:11 +0300

    Добавьте файлы проекта.

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPannel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.neighborsComboBox = new System.Windows.Forms.ComboBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.candidateButton = new System.Windows.Forms.Button();
+            this.bluesButton = new System.Windows.Forms.Button();
+            this.redsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPannel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonPannel
+            // 
+            this.buttonPannel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.buttonPannel.Controls.Add(this.label1);
+            this.buttonPannel.Controls.Add(this.button1);
+            this.buttonPannel.Controls.Add(this.neighborsComboBox);
+            this.buttonPannel.Controls.Add(this.calculateButton);
+            this.buttonPannel.Controls.Add(this.clearButton);
+            this.buttonPannel.Controls.Add(this.candidateButton);
+            this.buttonPannel.Controls.Add(this.bluesButton);
+            this.buttonPannel.Controls.Add(this.redsButton);
+            this.buttonPannel.Location = new System.Drawing.Point(9, 9);
+            this.buttonPannel.Name = "buttonPannel";
+            this.buttonPannel.Size = new System.Drawing.Size(190, 396);
+            this.buttonPannel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 34);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // neighborsComboBox
+            // 
+            this.neighborsComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.neighborsComboBox.FormattingEnabled = true;
+            this.neighborsComboBox.Items.AddRange(new object[] {
+            "5",
+            "9",
+            "15"});
+            this.neighborsComboBox.Location = new System.Drawing.Point(14, 187);
+            this.neighborsComboBox.Name = "neighborsComboBox";
+            this.neighborsComboBox.Size = new System.Drawing.Size(159, 33);
+            this.neighborsComboBox.TabIndex = 8;
+            this.neighborsComboBox.Text = "5";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(14, 281);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(159, 35);
+            this.calculateButton.TabIndex = 7;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(14, 333);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(159, 35);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear All";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // candidateButton
+            // 
+            this.candidateButton.Location = new System.Drawing.Point(14, 114);
+            this.candidateButton.Name = "candidateButton";
+            this.candidateButton.Size = new System.Drawing.Size(159, 35);
+            this.candidateButton.TabIndex = 2;
+            this.candidateButton.Text = "Candidate";
+            this.candidateButton.UseVisualStyleBackColor = true;
+            this.candidateButton.Click += new System.EventHandler(this.candidateButton_Click);
+            // 
+            // bluesButton
+            // 
+            this.bluesButton.Location = new System.Drawing.Point(14, 73);
+            this.bluesButton.Name = "bluesButton";
+            this.bluesButton.Size = new System.Drawing.Size(159, 35);
+            this.bluesButton.TabIndex = 1;
+            this.bluesButton.Text = "Blues";
+            this.bluesButton.UseVisualStyleBackColor = true;
+            this.bluesButton.Click += new System.EventHandler(this.bluesButton_Click);
+            // 
+            // redsButton
+            // 
+            this.redsButton.Location = new System.Drawing.Point(14, 30);
+            this.redsButton.Name = "redsButton";
+            this.redsButton.Size = new System.Drawing.Size(159, 35);
+            this.redsButton.TabIndex = 0;
+            this.redsButton.Text = "Reds";
+            this.redsButton.UseVisualStyleBackColor = true;
+            this.redsButton.Click += new System.EventHandler(this.redsButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Neibourghs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(642, 411);
+            this.Controls.Add(this.buttonPannel);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.MaximumSize = new System.Drawing.Size(1020, 760);
+            this.MinimumSize = new System.Drawing.Size(450, 450);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flying Points";
+            this.buttonPannel.ResumeLayout(false);
+            this.buttonPannel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel buttonPannel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button bluesButton;
+        private System.Windows.Forms.Button redsButton;
+        private System.Windows.Forms.Button candidateButton;
+        private System.Windows.Forms.ComboBox neighborsComboBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        private int closestNeighbours;
+
+        internal Point iPointToDrag;
+        bool bDragFlag;
+
+        private int searchRadius;
+        private Pen radiusPen;
+
+        int formWidth, formHeight;
+
+        internal int radius; internal int GetRadius() { return radius; }
+
+        List<Point> arRedPoints;
+        internal bool isRedsSelected;
+
+        List<Point> arBluePoints;
+        internal bool isBluesSelected;
+
+        private Point candidate;
+        internal bool isCandidateSelected;
+        internal bool isCandidateSet;
+
+
+        public Form1()
+        {
+            closestNeighbours = 5;
+          
+
+            InitializeComponent();
+            DoubleBuffered = true;
+            radius = 5;
+
+            searchRadius = 0;
+            radiusPen = new Pen(Color.White, 1);
+
+            isRedsSelected = true;
+            bDragFlag = false;
+            formWidth = this.Width;
+            formHeight = this.Height;
+
+            arRedPoints = new List<Point>();
+            isRedsSelected = true;
+
+            arBluePoints = new List<Point>();
+
+            isCandidateSet = false;
+
+            Paint += new PaintEventHandler(Form1_Paint);
+            MouseDown += button1_MouseDown;
+
+
+            KeyPreview = true;
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+            MouseUp += MouseUpper;
+
+            MouseMove += new MouseEventHandler(MouseMoving);
+  
+        }
+
+        void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            try
+            {        
+                
+                    foreach (var x in arRedPoints)
+                    {
+                        int xCentered = x.X - radius;
+                        int yCentered = x.Y - radius;
+
+                        g.FillEllipse(Brushes.Red, new Rectangle(xCentered, yCentered, radius * 2, radius * 2));
+                        
+                    }
+
+                    foreach (var x in arBluePoints)
+                    {
+                        int xCentered = x.X - radius;
+                        int yCentered = x.Y - radius;
+
+                        g.FillEllipse(Brushes.Blue, new Rectangle(xCentered, yCentered, radius * 2, radius * 2));
+
+                    }
+
+                    if (isCandidateSet)
+                    {
+                        int xCentered = candidate.X - radius;
+                        int yCentered = candidate.Y - radius;
+                        g.FillEllipse(Brushes.White, new Rectangle(xCentered, yCentered, radius * 2, radius * 2));
+                        g.DrawEllipse(radiusPen
+                            , candidate.X - searchRadius, candidate.Y - searchRadius
+                            , searchRadius * 2, searchRadius * 2);
+
+
+                      }
+
+
+            }
+            catch (Exception) { g.Clear(System.Drawing.SystemColors.ActiveCaption); }
+        }
+
+       
+
+        void ClearScreen()
+        {
+            arRedPoints = new List<Point>();
+            arBluePoints = new List<Point>();
+            isCandidateSet = false;
+            Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ClearScreen();
+        }
+        void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+            for (int i = 0; i < arRedPoints.Count ; i++)
+            {
+                if (((arRedPoints[i].X-radius < e.Location.X) && (arRedPoints[i].X + 2 * radius > e.Location.X)) &&
+                    ((arRedPoints[i].Y-radius < e.Location.Y) && (arRedPoints[i].Y + 2 * radius > e.Location.Y)))
+                {
+                    setReds();
+                    bDragFlag =true;
+                    Cursor = Cursors.Hand;
+                    iPointToDrag = arRedPoints[i];
+                    arRedPoints.Remove(iPointToDrag);
+                    Refresh();
+                    return;
+                }
+            }
+            for (int i = 0; i < arBluePoints.Count; i++)
+            {
+                if (((arBluePoints[i].X - radius < e.Location.X) && (arBluePoints[i].X + 2 * radius > e.Location.X)) &&
+                    ((arBluePoints[i].Y - radius < e.Location.Y) && (arBluePoints[i].Y + 2 * radius > e.Location.Y)))
+                {
+                    setBlues();
+                    bDragFlag = true;
+                    Cursor = Cursors.Hand;
+                    iPointToDrag = arBluePoints[i];
+                    arBluePoints.Remove(iPointToDrag);
+                    Refresh();
+                    return;
+                }
+            }
+            
+            if(isRedsSelected) 
+            {arRedPoints.Add(new Point(e.Location.X, e.Location.Y)); Refresh();return;}
+            if(isBluesSelected)
+            {arBluePoints.Add(new Point(e.Location.X, e.Location.Y)); Refresh();return;}
+            
+            if (isCandidateSelected)
+            {
+                candidate = new Point(e.Location.X, e.Location.Y);
+                isCandidateSet = true;
+            }
+            Refresh();
+        }
+
+        void MouseMoving(object sender, MouseEventArgs e)
+        {
+            if (bDragFlag)
+            {
+                iPointToDrag.X = e.Location.X;
+                iPointToDrag.Y = e.Location.Y;                             
+
+            }
+        }
+
+        void MouseUpper(object sender, MouseEventArgs e)
+        {
+            if (bDragFlag)
+            {
+                Cursor = Cursors.Cross;
+                if(isRedsSelected) arRedPoints.Add(iPointToDrag);
+                if(isBluesSelected) arBluePoints.Add(iPointToDrag);
+                bDragFlag = false;
+                Refresh();
+            }
+        }
+
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+             switch (e.KeyCode)
+              {
+                
+                case (Keys.Escape):
+                    ClearScreen(); break;
+                case (Keys.Oemplus):
+               
+                
+                default:break;
+              }
+
+               e.Handled = true;
+          
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+          
+            Refresh();
+
+
+        }
+        
+
+
+        private void redsButton_Click(object sender, EventArgs e)
+        {
+            setReds();
+        }
+
+        void setReds()
+        {
+            isCandidateSelected = false;
+            isBluesSelected = false;
+            isRedsSelected = true;
+        }
+
+        private void bluesButton_Click(object sender, EventArgs e)
+        {
+            setBlues();
+        }
+
+        void setBlues()
+        {
+            isCandidateSelected = false;
+            isBluesSelected = true;
+            isRedsSelected = false;
+        }
+        private void candidateButton_Click(object sender, EventArgs e)
+        {
+            setCandidate();
+        }
+
+        void setCandidate()
+
+        {
+            isCandidateSelected = true;
+            isBluesSelected = false;
+            isRedsSelected = false;
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            getResult();
+        }
+
+        private void getResult()
+        {
+            String resultMessage = "";
+            if (isCandidateSet)
+            {
+                
+                List<Neighbour> neighbours = new List<Neighbour>();
+                int id = 0;
+                foreach(var x in arRedPoints)
+                    neighbours.Add(new Neighbour(++id,matchDistance(candidate,x),"red"));
+                foreach (var x in arBluePoints)
+                    neighbours.Add(new Neighbour(++id, matchDistance(candidate, x), "blue"));
+
+                neighbours = neighbours.OrderBy(x => x.distanceToCandidate)
+                    .Take(closestNeighbours).ToList();
+
+
+                int longestDistance = (int)neighbours.Last().distanceToCandidate+radius;
+                for (searchRadius = 0; searchRadius < longestDistance; searchRadius++) Refresh();
+                searchRadius = 0;
+
+                int reds = neighbours.Where(x => x.label == "red").Count();
+                int blues = neighbours.Where(x => x.label == "blue").Count();
+
+                
+                if (reds > blues) resultMessage = "Candidate is classified as red";
+                if (reds < blues) resultMessage = "Candidate is classified as blue";
+                if (reds == blues)
+                {
+                    if (neighbours.First().label=="red") resultMessage = "Candidate is classified as red";
+                    if (neighbours.First().label == "blue") resultMessage = "Candidate is classified as blue";
+                }
+
+            }
+            else
+            {
+                resultMessage = "Please, set a candidate";
+            }
+
+            MessageBox.Show(
+                resultMessage,
+                "Result:",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (int.TryParse(neighborsComboBox.Text, out closestNeighbours)) ;
+
+            else neighborsComboBox.Text = "Put here int";
+        }
+
+        internal double matchDistance(Point a, Point b)
+        {
+            double xdif = a.X - b.X;
+            double ydif = a.Y - b.Y;
+            return Math.Sqrt(xdif * xdif + ydif * ydif);
+        }
+
+    }
+
+    class myPointDelta
+    {
+        public int x, y;
+        public myPointDelta(int X, int Y) { x = X; y = Y; }
+    }
+    class Neighbour
+    {
+        public int id;
+        public double distanceToCandidate;
+        public String label;
+
+        public Neighbour(int id, double dist, String label)
+        {
+            this.id = id;
+            this.distanceToCandidate = dist;
+            this.label = label;
+        }
+    }
+
+}
